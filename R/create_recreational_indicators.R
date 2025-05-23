5e79129614986bf42f7c656e543f5f2647e78a0b
Maxwell Grezlik <maxwell.grezlik@noaa.gov>
2025-05-23 16:46:32 -0400

    fixed typo (lowercase s at end of SPECIES)

--- R/create_recreational_indicators.R
+++ R/create_recreational_indicators.R
@@ -175,7 +175,7 @@ create_rec_trips <- function(dir,
                   INDICATOR_TYPE = "Socioeconomic",
                   INDICATOR_NAME = "rec_trips",
                   INDICATOR_UNITS = "number") |>
-    dplyr::select(YEAR, DATA_VALUE, CATEGORY, INDICATOR_TYPE, INDICATOR_NAME, INDICATOR_UNITS, SPECIEs) 
+    dplyr::select(YEAR, DATA_VALUE, CATEGORY, INDICATOR_TYPE, INDICATOR_NAME, INDICATOR_UNITS, SPECIES) 
   
   return(output)
   
