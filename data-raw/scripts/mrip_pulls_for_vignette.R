35cebf30f8feadc9a0187dc0dce84a429da5ff2e
Stephanie Owen <stephanie.owen@noaa.gov>
2025-07-07 13:15:09 -0500

    finish mrip vignette with new data query functions

--- data-raw/scripts/mrip_pulls_for_vignette.R
+++ data-raw/scripts/mrip_pulls_for_vignette.R
@@ -4,7 +4,9 @@ bsb_catch <- get_mrip_catch(species = "BLACK SEA BASS", type = "all")
 catch <- bsb_catch$data |>
   dplyr::rename(YEAR = Year,
                 DATA_VALUE = `Total Catch (A+B1+B2)`) |>
-  dplyr::mutate(INDICATOR_NAME = 'Recreational Catch')
+  dplyr::mutate(INDICATOR_NAME = 'Recreational Catch') 
+
+catch$DATA_VALUE <- as.numeric(gsub(",","",catch$DATA_VALUE))
 
 plt_indicator(catch)
 
@@ -16,6 +18,9 @@ landings <- bsb_landings$data |>
                 DATA_VALUE = `Total Harvest (A+B1)`) |>
   dplyr::mutate(INDICATOR_NAME = 'Recreational Landings')
 
+landings$DATA_VALUE <- as.numeric(gsub(",","",landings$DATA_VALUE))
+
+head(landings)
 plt_indicator(landings)
 
 #trips
@@ -32,20 +37,35 @@ trips2_2022 <- get_mrip_trips(species = "BLACK SEA BASS", region = "Mid-Atlantic
 trips2_2023 <- get_mrip_trips(species = "BLACK SEA BASS", region = "Mid-Atlantic", year = '2023')
 trips2_2024 <- get_mrip_trips(species = "BLACK SEA BASS", region = "Mid-Atlantic", year = '2024')
 
-trips <- rbind(trips_2020$data,
+n_trips <- rbind(trips_2020$data,
                trips_2021$data,
                trips_2022$data,
                trips_2023$data,
-               trips_2024$data,
+               trips_2024$data)
+
+mid_trips <- rbind(
                trips2_2020$data,
                trips2_2021$data,
                trips2_2022$data,
                trips2_2023$data,
                trips2_2024$data)
 
-trips <- trips |>
+n_trips <- n_trips |>
   dplyr::rename(YEAR = Year,
                 DATA_VALUE = `Directed Trips`) |>
   dplyr::mutate(INDICATOR_NAME = 'Recreational Trips')
 
-pak::pak("noaa-edab/ecodata") 
+n_trips$DATA_VALUE <- as.numeric(gsub(",","",n_trips$DATA_VALUE))
+
+head(n_trips)
+plt_indicator(n_trips)
+
+mid_trips <- mid_trips |>
+  dplyr::rename(YEAR = Year,
+                DATA_VALUE = `Directed Trips`) |>
+  dplyr::mutate(INDICATOR_NAME = 'Recreational Trips')
+
+mid_trips$DATA_VALUE <- as.numeric(gsub(",","",mid_trips$DATA_VALUE))
+
+head(mid_trips)
+plt_indicator(mid_trips)
