1668ddb767a6fa2211d992041702f3f2fe9a696d
Abby Tyrell <77738923+atyrell3@users.noreply.github.com>
2025-11-21 15:40:31 -0500

    merge files from dev

--- R/plot_condition.R
+++ R/plot_condition.R
@@ -1,62 +1,59 @@
-#' Plot Condition
+#' Plot Condition 
 #'
 #' @param data A data frame of species condition from `species_condition(..., output = "soe")`
 #' @param var species of interest to plot
+#' @param return Boolean, if TRUE will return the ggplot object
+#' @importFrom rlang .data
 #' @return A ggplot
-#'
 #' @export
 
-plot_condition <- function(data, var) {
+plot_condition <- function(data, 
+                           var,
+                           return = TRUE){
   condition <- data |>
-    dplyr::select(YEAR, Species, EPU, DATA_VALUE) |>
-    dplyr::group_by(Species) |>
-    dplyr::mutate(scaleCond = scale(DATA_VALUE, scale = T, center = T))
-
-  xs <- quantile(condition$scaleCond, seq(0, 1, length.out = 6), na.rm = TRUE)
-
+    dplyr::select(.data$YEAR,
+                  .data$Species,
+                  .data$EPU,
+                  .data$DATA_VALUE) |>
+    dplyr::group_by(.data$Species) |>
+    dplyr::mutate(scaleCond = scale(.data$DATA_VALUE,scale =T,center=T))
+  
+  xs <- stats::quantile(condition$scaleCond, seq(0,1, length.out = 6), na.rm = TRUE)
+  
   condition <- condition |>
-    dplyr::mutate(
-      category = cut(
-        scaleCond,
-        breaks = xs,
-        labels = c(
-          "Poor Condition",
-          "Below Average",
-          "Neutral",
-          "Above Average",
-          "Good Condition"
-        ),
-        include.lowest = TRUE
-      )
-    )
-
+    dplyr::mutate(category = cut(.data$scaleCond,
+                                 breaks = xs,
+                                 labels = c( "Poor Condition",
+                                             "Below Average",
+                                             "Neutral",
+                                             "Above Average",
+                                             "Good Condition"),
+                                 include.lowest = TRUE))
+  
   condition <- condition |>
-    dplyr::filter(Species %in% var) |>
+    dplyr::filter(.data$Species %in% var) |>
     dplyr::ungroup() |>
-    dplyr::arrange(YEAR) |>
-    dplyr::group_by(EPU) |>
-    dplyr::mutate(
-      mean = mean(DATA_VALUE, na.rm = TRUE),
-      sd = sd(DATA_VALUE, na.rm = TRUE)
-    ) |>
-    ggplot2::ggplot(ggplot2::aes(
-      x = YEAR,
-      y = DATA_VALUE,
-      color = category,
-      shape = EPU
+    dplyr::arrange(.data$YEAR) |>
+    dplyr::group_by(.data$EPU) |>
+    dplyr::mutate(mean = mean(.data$DATA_VALUE, na.rm = TRUE),
+                  sd = stats::sd(.data$DATA_VALUE, na.rm = TRUE)) |>
+    ggplot2::ggplot(ggplot2::aes(x = .data$YEAR,
+                                 y = .data$DATA_VALUE,
+                                 color = .data$category,
+                                 shape = .data$EPU
     )) +
     ggplot2::geom_path(color = "black", lty = 2, alpha = 0.5) +
     ggplot2::geom_point(cex = 3) +
     ggplot2::xlim(c(1989, 2024)) +
     ggplot2::theme_classic(base_size = 16) +
-    ggplot2::theme(
-      strip.text = ggplot2::element_text(size = 16),
-      axis.title.x = ggplot2::element_blank(),
-      aspect.ratio = 0.4,
-      legend.direction = "vertical",
-      legend.box = "horizontal"
-    ) +
+    ggplot2::theme(strip.text = ggplot2::element_text(size = 16),
+                   axis.title.x = ggplot2::element_blank(),
+                   aspect.ratio = 0.4,
+                   legend.direction = "vertical",
+                   legend.box = "horizontal") +
     viridis::scale_color_viridis(discrete = TRUE)
-
+  
   return(condition)
 }
+
+
