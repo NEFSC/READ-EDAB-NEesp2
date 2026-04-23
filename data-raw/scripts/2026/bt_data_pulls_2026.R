5d1129dfcefcac4fc5f5ea60f9eb742aa567cb04
Stephanie Owen <stephanie.owen@noaa.gov>
2026-04-23 14:34:11 -0500

    calc bottom temp over entire stock area

--- data-raw/scripts/2026/bt_data_pulls_2026.R
+++ data-raw/scripts/2026/bt_data_pulls_2026.R
@@ -15,12 +15,14 @@ fishbot <- ncdf4::nc_open(here::here('data-raw/2026/fishbot_2000_2026.nc'))
 ### filepaths from container
 glorys <- '//nefscdata/SOE_ESP_Data/READ-EDAB-NEesp2/data-raw/2026/glorys_2021_2026.nc' # |>
 # terra::vect()
+glorys <- here::here('data-raw/2026/glorys_2021_2026.nc') # |>
 
 hubert <- "//nefscdata/SOE_ESP_Data/READ-EDAB-NEesp2/data-raw/2026/duPontavice_bottom_temp_1959_2021.nc" #|>
 # terra::vect()
+hubert <- here::here('data-raw/2026/duPontavice_bottom_temp_1959_2021.nc') #|>
 
-fishbot <- '//nefscdata/SOE_ESP_Data/READ-EDAB-NEesp2/data-raw/2026/fishbot_2000_2026.nc' |>
-  terra::vect()
+fishbot <- '//nefscdata/SOE_ESP_Data/READ-EDAB-NEesp2/data-raw/2026/fishbot_2000_2026.nc' #|>
+  # terra::vect()
 
 
 ## create stock shapefile from strata provided
@@ -35,78 +37,26 @@ create_shp <- function(strata, orig_shp = shp) {
   return(shp_out)
 }
 
-### HALIBUT
-
-exp <- knitr::knit_expand(
-  file = here::here("data-raw/scripts/2026/spatial_code_template.R"),
-  species = "halibut",
-  strata = 'c(
-    "01130",
-    "01140",
-    "01150",
-    "01160",
-    "01170",
-    "01180",
-    "01190",
-    "01200",
-    "01210",
-    "01220",
-    "01230",
-    "01240",
-    "01250",
-    "01260",
-    "01270",
-    "01280",
-    "01290",
-    "01300",
-    "01360",
-    "01370",
-    "01380",
-    "01390",
-    "01400"
-  )'
-)
-
-eval(parse(text = exp))
-
-halibut_shp <- create_shp(
-  strata = c(
-    "01130",
-    "01140",
-    "01150",
-    "01160",
-    "01170",
-    "01180",
-    "01190",
-    "01200",
-    "01210",
-    "01220",
-    "01230",
-    "01240",
-    "01250",
-    "01260",
-    "01270",
-    "01280",
-    "01290",
-    "01300",
-    "01360",
-    "01370",
-    "01380",
-    "01390",
-    "01400"
+eval_spatial <- function(species, strata_nums) {
+  exp <- knitr::knit_expand(
+    file = here::here("data-raw/scripts/2026/spatial_code_template.R"),
+    species = species,
+    strata = strata_nums
   )
-)
+  
+  eval(parse(text = exp))
+}
 
-#not recognizing var.name as "temperature"
-fishbot_halibut <- create_spatial_indicator(
-  indicator_name = "fishbot_bottomT",
-  units = "degC",
-  data.in = c(here::here('data-raw/2026', 'fishbot_2000_2026.nc')),
-  file.time = 'annual',
-  output.files = c(here::here('data-raw', 'fishbot_halibut_bottomT.nc')),
-  shp.file = here::here('data-raw/shapefiles', 'BTS_STRATA.shp'),
-  var.name = 'temperature',
-  area.names = c(
+eval_spatial(species = input_data[[1]]$species,
+             strata_nums = input_data[[1]]$strat)
+
+purrr::map(input_data,
+           ~ eval_spatial(species = .x$species,
+                          strata_nums = .x$strat))
+
+input_data <- list(
+  halibut = list(species = "halibut",
+  strat = 'c(
     "01130",
     "01140",
     "01150",
@@ -130,73 +80,9 @@ fishbot_halibut <- create_spatial_indicator(
     "01380",
     "01390",
     "01400"
-  ),
-  statistic = 'mean',
-  agg.time = 'days',
-  tz = NA,
-  touches = TRUE,
-  write.out = F
-)
-
-bt_halibut <- create_spatial_indicator(
-  indicator_name = "bottomT",
-  units = "degC",
-  data.in = c(here::here(
-    'data-raw/2026',
-    'duPontavice_bottom_temp_1959_2021.nc'
-  )),
-  file.time = 'annual',
-  output.files = c(here::here('data-raw', 'halibut_bottomT.nc')),
-  shp.file = halibut_shp,
-  var.name = "sea_water_temperature_at_sea_floor",
-  area.names = "stock_area",
-  statistic = 'mean',
-  agg.time = 'months',
-  tz = NA,
-  touches = TRUE,
-  write.out = F
-)
-
-write.csv(
-  bt_halibut,
-  here::here('data-raw/2026', 'halibut_hubert_bottomT.csv'),
-  row.names = FALSE
-)
-
-glorys_halibut <- create_spatial_indicator(
-  indicator_name = "bottomT",
-  units = "degC",
-  data.in = c(here::here('data-raw/2026', 'glorys_2021_2026.nc')),
-  file.time = 'annual',
-  output.files = c(here::here('data-raw', 'halibut_glorys.nc')),
-  shp.file = halibut_shp,
-  var.name = "bottomT",
-  area.names = "stock_area",
-  statistic = 'mean',
-  agg.time = 'months',
-  tz = NA,
-  touches = TRUE,
-  write.out = F
-)
-write.csv(
-  glorys_halibut,
-  here::here('data-raw/2026', 'halibut_glorys_bottomT.csv'),
-  row.names = FALSE
-)
-
-### POLLOCK
-bt_pollock <- create_spatial_indicator(
-  indicator_name = "bottomT",
-  units = "degC",
-  data.in = c(here::here(
-    'data-raw/2026',
-    'duPontavice_bottom_temp_1959_2021.nc'
-  )),
-  file.time = 'annual',
-  output.files = c(here::here('data-raw', 'pollock_bottomT.nc')),
-  shp.file = here::here('data-raw/shapefiles', 'BTS_STRATA.shp'),
-  var.name = "sea_water_temperature_at_sea_floor",
-  area.names = c(
+  )'),
+  pollock = list(species = "pollock",
+                 strat = 'c(
     "01130",
     "01140",
     "01150",
@@ -220,78 +106,9 @@ bt_pollock <- create_spatial_indicator(
     "01380",
     "01390",
     "01400"
-  ),
-  statistic = 'mean',
-  agg.time = 'days',
-  tz = NA,
-  touches = TRUE,
-  write.out = F
-)
-write.csv(
-  bt_pollock,
-  here::here('data-raw/2026', 'pollock_hubert_bottomT.csv'),
-  row.names = FALSE
-)
-
-glorys_pollock <- create_spatial_indicator(
-  indicator_name = "bottomT",
-  units = "degC",
-  data.in = c(here::here('data-raw/2026', 'glorys_2021_2026.nc')),
-  file.time = 'annual',
-  output.files = c(here::here('data-raw', 'pollock_glorys.nc')),
-  shp.file = here::here('data-raw/shapefiles', 'BTS_STRATA.shp'),
-  var.name = "bottomT",
-  area.names = c(
-    "01130",
-    "01140",
-    "01150",
-    "01160",
-    "01170",
-    "01180",
-    "01190",
-    "01200",
-    "01210",
-    "01220",
-    "01230",
-    "01240",
-    "01250",
-    "01260",
-    "01270",
-    "01280",
-    "01290",
-    "01300",
-    "01360",
-    "01370",
-    "01380",
-    "01390",
-    "01400"
-  ),
-  statistic = 'mean',
-  agg.time = 'days',
-  tz = NA,
-  touches = TRUE,
-  write.out = F
-)
-write.csv(
-  glorys_pollock,
-  here::here('data-raw/2026', 'pollock_glorys_bottomT.csv'),
-  row.names = FALSE
-)
-
-
-### RED HAKE (AS UNIT, NEED STRATA FOR N/S)
-bt_redhake <- create_spatial_indicator(
-  indicator_name = "bottomT",
-  units = "degC",
-  data.in = c(here::here(
-    'data-raw/2026',
-    'duPontavice_bottom_temp_1959_2021.nc'
-  )),
-  file.time = 'annual',
-  output.files = c(here::here('data-raw', 'redhake_bottomT.nc')),
-  shp.file = here::here('data-raw/shapefiles', 'BTS_STRATA.shp'),
-  var.name = "sea_water_temperature_at_sea_floor",
-  area.names = c(
+  )'),
+  red_hake = list(species = "red_hake",
+                  strat = 'c(
     "01200",
     "01210",
     "01220",
@@ -343,28 +160,9 @@ bt_redhake <- create_spatial_indicator(
     "01740",
     "01750",
     "01760"
-  ),
-  statistic = 'mean',
-  agg.time = 'days',
-  tz = NA,
-  touches = TRUE,
-  write.out = F
-)
-write.csv(
-  bt_redhake,
-  here::here('data-raw/2026', 'redhake_hubert_bottomT.csv'),
-  row.names = FALSE
-)
-
-glorys_redhake <- create_spatial_indicator(
-  indicator_name = "bottomT",
-  units = "degC",
-  data.in = c(here::here('data-raw/2026', 'glorys_2021_2026.nc')),
-  file.time = 'annual',
-  output.files = c(here::here('data-raw', 'redhake_glorys.nc')),
-  shp.file = here::here('data-raw/shapefiles', 'BTS_STRATA.shp'),
-  var.name = "bottomT",
-  area.names = c(
+  )'),
+  silver_hake = list(species = "silver_hake",
+                     strat = 'c(
     "01200",
     "01210",
     "01220",
@@ -416,168 +214,9 @@ glorys_redhake <- create_spatial_indicator(
     "01740",
     "01750",
     "01760"
-  ),
-  statistic = 'mean',
-  agg.time = 'days',
-  tz = NA,
-  touches = TRUE,
-  write.out = F
-)
-write.csv(
-  glorys_redhake,
-  here::here('data-raw/2026', 'redhake_glorys_bottomT.csv'),
-  row.names = FALSE
-)
+  )'))
 
-### SILVER HAKE (AS UNIT, NEED STRATA FOR N/S)
-bt_silverhake <- create_spatial_indicator(
-  indicator_name = "bottomT",
-  units = "degC",
-  data.in = c(here::here(
-    'data-raw/2026',
-    'duPontavice_bottom_temp_1959_2021.nc'
-  )),
-  file.time = 'annual',
-  output.files = c(here::here('data-raw', 'silverhake_bottomT.nc')),
-  shp.file = here::here('data-raw/shapefiles', 'BTS_STRATA.shp'),
-  var.name = "sea_water_temperature_at_sea_floor",
-  area.names = c(
-    "01200",
-    "01210",
-    "01220",
-    "01230",
-    "01240",
-    "01250",
-    "01260",
-    "01270",
-    "01280",
-    "01290",
-    "01300",
-    "01360",
-    "01370",
-    "01380",
-    "01390",
-    "01400",
-    "01010",
-    "01020",
-    "01030",
-    "01040",
-    "01050",
-    "01060",
-    "01070",
-    "01080",
-    "01090",
-    "01100",
-    "01110",
-    "01120",
-    "01130",
-    "01140",
-    "01150",
-    "01160",
-    "01170",
-    "01180",
-    "01190",
-    "01610",
-    "01620",
-    "01630",
-    "01640",
-    "01650",
-    "01660",
-    "01670",
-    "01680",
-    "01690",
-    "01700",
-    "01710",
-    "01720",
-    "01730",
-    "01740",
-    "01750",
-    "01760"
-  ),
-  statistic = 'mean',
-  agg.time = 'days',
-  tz = NA,
-  touches = TRUE,
-  write.out = F
-)
-write.csv(
-  bt_silverhake,
-  here::here('data-raw/2026', 'silverhake_hubert_bottomT.csv'),
-  row.names = FALSE
-)
 
-glorys_silverhake <- create_spatial_indicator(
-  indicator_name = "bottomT",
-  units = "degC",
-  data.in = c(here::here('data-raw/2026', 'glorys_2021_2026.nc')),
-  file.time = 'annual',
-  output.files = c(here::here('data-raw', 'silverhake_glorys.nc')),
-  shp.file = here::here('data-raw/shapefiles', 'BTS_STRATA.shp'),
-  var.name = "bottomT",
-  area.names = c(
-    "01200",
-    "01210",
-    "01220",
-    "01230",
-    "01240",
-    "01250",
-    "01260",
-    "01270",
-    "01280",
-    "01290",
-    "01300",
-    "01360",
-    "01370",
-    "01380",
-    "01390",
-    "01400",
-    "01010",
-    "01020",
-    "01030",
-    "01040",
-    "01050",
-    "01060",
-    "01070",
-    "01080",
-    "01090",
-    "01100",
-    "01110",
-    "01120",
-    "01130",
-    "01140",
-    "01150",
-    "01160",
-    "01170",
-    "01180",
-    "01190",
-    "01610",
-    "01620",
-    "01630",
-    "01640",
-    "01650",
-    "01660",
-    "01670",
-    "01680",
-    "01690",
-    "01700",
-    "01710",
-    "01720",
-    "01730",
-    "01740",
-    "01750",
-    "01760"
-  ),
-  statistic = 'mean',
-  agg.time = 'days',
-  tz = NA,
-  touches = TRUE,
-  write.out = F
-)
-write.csv(
-  glorys_redhake,
-  here::here('data-raw/2026', 'silverhake_glorys_bottomT.csv'),
-  row.names = FALSE
-)
 
 ### WITCH FLOUNDER
 # USING BTS_STRATA.shp ERRORS OUT WITH A CROPPING ERROR (Error: [crop] cannot crop a SpatRaster with an empty extent, [mask] CRS do not match )
--- data-raw/scripts/2026/spatial_code_template.R
+++ data-raw/scripts/2026/spatial_code_template.R
@@ -2,30 +2,30 @@ species_shp <- create_shp(
   strata = {{ strata }},
   orig_shp = shp
 )
-#
-# bt <- create_spatial_indicator(
-#   indicator_name = "bottomT",
-#   units = "degC",
-#   data.in = hubert,
-#   file.time = 'annual',
-#   output.files = c(here::here('data-raw', '{{ species }}_bottomT.nc')),
-#   shp.file = species_shp,
-#   var.name = "sea_water_temperature_at_sea_floor",
-#   area.names = "stock_area",
-#   statistic = 'mean',
-#   agg.time = 'months',
-#   tz = NA,
-#   touches = TRUE,
-#   write.out = F
-# )
-#
-# write.csv(
-#   bt,
-#   here::here('data-raw/2026', '{{ species }}_hubert_bottomT.csv'),
-#   row.names = FALSE
-# )
 
-glorys <- create_spatial_indicator(
+bt <- create_spatial_indicator(
+  indicator_name = "bottomT",
+  units = "degC",
+  data.in = hubert,
+  file.time = 'annual',
+  output.files = c(here::here('data-raw', '{{ species }}_bottomT.nc')),
+  shp.file = species_shp,
+  var.name = "sea_water_temperature_at_sea_floor",
+  area.names = "stock_area",
+  statistic = 'mean',
+  agg.time = 'days',
+  tz = NA,
+  touches = TRUE,
+  write.out = F
+)
+
+write.csv(
+  bt,
+  here::here('data-raw/2026', '{{ species }}_hubert_bottomT.csv'),
+  row.names = FALSE
+)
+
+glorys_species <- create_spatial_indicator(
   indicator_name = "bottomT",
   units = "degC",
   data.in = glorys,
@@ -35,13 +35,55 @@ glorys <- create_spatial_indicator(
   var.name = "bottomT",
   area.names = "stock_area",
   statistic = 'mean',
-  agg.time = 'months',
+  agg.time = 'days',
   tz = NA,
   touches = TRUE,
   write.out = F
 )
 write.csv(
-  glorys,
+  glorys_species,
   here::here('data-raw/2026', '{{ species }}_glorys_bottomT.csv'),
   row.names = FALSE
 )
+
+
+# #not recognizing var.name as "temperature"
+# fishbot_halibut <- create_spatial_indicator(
+#   indicator_name = "fishbot_bottomT",
+#   units = "degC",
+#   data.in = c(here::here('data-raw/2026', 'fishbot_2000_2026.nc')),
+#   file.time = 'annual',
+#   output.files = c(here::here('data-raw', 'fishbot_halibut_bottomT.nc')),
+#   shp.file = here::here('data-raw/shapefiles', 'BTS_STRATA.shp'),
+#   var.name = 'temperature',
+#   area.names = c(
+#     "01130",
+#     "01140",
+#     "01150",
+#     "01160",
+#     "01170",
+#     "01180",
+#     "01190",
+#     "01200",
+#     "01210",
+#     "01220",
+#     "01230",
+#     "01240",
+#     "01250",
+#     "01260",
+#     "01270",
+#     "01280",
+#     "01290",
+#     "01300",
+#     "01360",
+#     "01370",
+#     "01380",
+#     "01390",
+#     "01400"
+#   ),
+#   statistic = 'mean',
+#   agg.time = 'days',
+#   tz = NA,
+#   touches = TRUE,
+#   write.out = F
+# )
